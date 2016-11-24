74fc08771876bba9795db042e3a75badcbe66684
soichit <soichit@uw.edu>
2016-11-23 23:26:08 -0800

    started dynamic layout

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
   selector: 'app-projects',
@@ -7,17 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   private selectionIndex: number;
-  isSpecial:any;
+  private isSpecial:any;
+  private data: Object;
+  private projects: any;
 
-  constructor() { }
+  constructor(private http: Http) { 
+    http.get("../assets/projects.json")
+        .map(res => res.json())
+        .subscribe(data => this.data = data,
+                    err => console.log(err),
+                    () => console.log('Completed'));
+  }
 
   ngOnInit() {
     this.selectionIndex = 0;
     this.isSpecial = true;
+    //this.projects = JSON.parse(JSON.stringify(this.data));
+    console.log(this.data);
+    console.log(this.projects);
   }
 
   onClick(index: number) {
     this.selectionIndex = index;
+    console.log(this.data);
+    console.log(this.data[0].title);
+    console.log(this.projects);
+    this.data = JSON.parse(JSON.stringify(this.data));
   }
 
   onClickLink() {
